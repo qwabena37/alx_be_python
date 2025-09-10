ab774327b56fadc893cd658ce1106e413ebfe160
qwabena37 <qjaymce37@gmail.com>
2025-09-10 16:32:21 +0000

    basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print('Addition of 10 and 5 is '+sum)
+
+Subtraction = number1 - number2
+print('Subtratction of 10 and 5 is '+ Subtraction)
+
+Multiplication = number1*number2
+print('Multiplication of 10 and 5 is '+ Multiplication)
