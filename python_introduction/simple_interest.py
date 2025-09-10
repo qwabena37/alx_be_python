1d5397c1335bf9f67f2a5387e5e88ef77bc920f8
qwabena37 <qjaymce37@gmail.com>
2025-09-10 18:29:58 +0000

    add rectangle_area.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,6 +3,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-interest = (principal * rate * time)
+interest = (principal*rate*time)
 interest = str(interest)
 print('The simple interest is: ' + interest)
