e18aea9a2e72ee32b704cf86864c7f53580d3d8c
James <qjaymce37@gmail.com>
2025-09-22 19:15:05 +0000

    add multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,13 +2,13 @@
 def main():
     try:
         # Ask the user to enter a number
-        number = int(input("Enter a number to see its multiplication table"))
+        number = int(input("Enter a number to see its multiplication table: "))
 
         print(f"\nMultiplication Table for {number}:\n")
         # Loop from 1 to 10
         for i in range(1, 11):
-            result = number * i
-            print(f"{number} x {i} = {result}")
+            product = number * i
+            print(f"{number} * {i} = {product}")
 
     except ValueError:
         print("Please enter a valid integer.")
