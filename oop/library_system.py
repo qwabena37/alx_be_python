8ccb26e6d2c3953879719804c41cc74cd6a119a5
James <qjaymce37@gmail.com>
2025-10-09 17:37:47 +0000

    add library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,10 +6,10 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}')"
+        return f"Book: {self.title}', '{self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
